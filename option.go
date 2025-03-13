7fad04b9204d13f753e8d470933528152fd7685b
2027 <51899594+trainlabs@users.noreply.github.com>
2025-03-13 13:48:59 +0800

    Create option.go

---
+++ option.go
@@ -0,0 +1,31 @@
+
+type Options struct {
+	WorkDir           string // 工作数据目录;
+	MemTableSize      int64  // 内存表最大限制;
+	SSTableMaxSz      int64  // SSSTable 最大限制,同上;
+	NumFlushMemtables int    // 刷盘队列大小;
+	BlockSize         uint32 // 数据块持久化时的大小;
+
+	BloomFalsePositive float64 // 布隆过滤器的容错率;
+
+	CacheNums int // 缓存元素个数, 缺省值默认 1024*10个;
+
+	ValueThreshold      int   // 进入vlog的value阈值;
+	ValueLogMaxEntries  int32 // vlogFile文件保存的entry最大数量;
+	ValueLogFileSize    int32 // vlogFile的文件大小;
+	VerifyValueChecksum bool  // 是否开启vlogFile的crc检查;
+
+	MaxBatchCount int64 // 批处理entry数量;
+	MaxBatchSize  int64 // 批处理entry总量大小;
+
+	// compact 合并相关
+	NumCompactors       int   // 合并协程数量;默认2;
+	BaseLevelSize       int64 // 基层中 所期望的文件大小;
+	LevelSizeMultiplier int   // 决定 level 之间期望 总体文件 size 比例, 默认是 10倍;
+	TableSizeMultiplier int   // 决定每层 文件 递增倍数;
+	BaseTableSize       int64 // 基层中 文件所期望的文件大小;
+	NumLevelZeroTables  int   // 第 0 层中,允许的表数量;
+	MaxLevelNum         int   // 最大层数,默认是 7 层;
+
+	DiscardStatsCh *chan map[uint32]int64 //  用于 compact 组件向 vlog 组件传递信息使用,在合并过程中,知道哪些文件是失效的,让vlog组件知道,方便其GC;
+}
